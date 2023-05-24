9a7d8aaa4a1645736919bb737729096ac9f9e980
Ayush <ayushjain201474@gmail.com>
2023-05-25 00:48:11 +0530

    Added footer-final component

--- app/Components/Footer.js
+++ app/Components/Footer.js
@@ -1,3 +1,17 @@
 export default function Footer() {
-  return <div className="h-[10vh] bg-black"></div>;
+  return (
+    <div className="h-auto bg-black md:mb-0 mb-[60px]">
+      <div className="flex flex-wrap flex-col md:flex-row justify-center sm:justify-between container py-10 px-10 mx-auto">
+        <p className="text-gray-500 mb-[10px] md:mb-0 text-center">
+          Copyright © {new Date().getFullYear()}. All rights reserved
+        </p>
+        <p className="text-center text-gray-500">
+          Coded with ❤️ by{" "}
+          <a className="text-white" href="www.linkedin.com/in/ayushj19">
+            Ayush
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
